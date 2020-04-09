0b220dfcf9d75900de5e67900d29809d91716c58
Vijay Sivagnanam <smvj007@gmail.com>
2020-04-09 01:52:33 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
+  name = "new VPC for Gaurav"
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
